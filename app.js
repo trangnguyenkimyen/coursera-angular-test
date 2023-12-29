a0e280db34a02ef1e1ec09316315974495968991
Kim Yen <Trangnguyenkimyen@gmail.com>
2023-12-29 15:41:46 +0700

    first init lecture6 - Name Calculator

--- app.js
+++ app.js
@@ -2,10 +2,23 @@
 (function () {
     'use-strict';
 
-    angular.module('myFirstApp', [])
+    angular.module('NameCalculator', [])
 
-        .controller('MyFirstController', function ($scope) {
-            $scope.name = 'Ky';
+        .controller('NameCalculatorController', function ($scope) {
+            $scope.name = '';
+            $scope.totalValue = 0;
+            $scope.displayNumeric = () => {
+                const count = calculateNumericforString($scope.name);
+                $scope.totalValue = count;
+            };
+
+            const calculateNumericforString = (string) => {
+                let totalStringValue = 0;
+                for (let i = 0; i < string.length; i++) {
+                    totalStringValue += string.charCodeAt(i);
+                }
+
+                return totalStringValue;
+            };
         })
-
 })();
