b52a2ec78d244e2a2eb6ceec0843e223f0f8c032
Kim Yen <Trangnguyenkimyen@gmail.com>
2024-01-11 12:20:56 +0700

    Attach  property to the function object

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-// app.js
-(function () {
-    'use-strict';
-
-
-
-})();
+!function () {
+    function e(e, n) {
+        e.name = "Ky",
+            e.upper = () => {
+                let o = n("uppercase");
+                e.name = o(e.name)
+            }
+    }
+    angular.module("DIApp", [])
+        .controller("DIController", e),
+        e.$inject = ["$scope", "$filter"]
+}();
