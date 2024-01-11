b7ff1ca521829e88ba9cdd666881bf1cafea48e5
Kim Yen <Trangnguyenkimyen@gmail.com>
2024-01-11 12:35:40 +0700

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 (function () {
     'use-strict';
 
+    angular.module('DIApp', [])
+        .controller('DIController', DIController);
 
+    function DIController($scope, $filter) {
+        $scope.name = 'Ky';
+        $scope.upper = function () {
+            const upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        }
+    }
 
 })();
