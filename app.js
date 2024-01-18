43738e68bb8965b98c4ea1e8b8ef697f77eae065
Kim Yen <Trangnguyenkimyen@gmail.com>
2024-01-18 20:05:47 +0700

    first init

--- app.js
+++ app.js
@@ -1,44 +1,12 @@
 (function () {
     'use-strict';
 
-    const shoppingList1 = [
+    const shoppingList = [
         "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
     ];
 
-    const shoppingList2 = [
-        {
-            name: "Milk",
-            quantity: "2"
-        },
-        {
-            name: "Donuts",
-            quantity: "200"
-        },
-        {
-            name: "Cookies",
-            quantity: "300"
-        },
-        {
-            name: "Chocolate",
-            quantity: "5"
-        }
-    ];
-
-
     const ShoppingListController = ($scope) => {
-        $scope.shoppingList1 = shoppingList1;
-        $scope.shoppingList2 = shoppingList2;
-
-        $scope.addToList = () => {
-            const newItem = {
-                name: $scope.newItemName,
-                quantity: $scope.newItemQuantity,
-            };
-
-            $scope.shoppingList2.push(newItem);
-            $scope.newItemName = '';
-            $scope.newItemQuantity = '';
-        };
+        $scope.shoppingList = shoppingList;
     };
 
 
