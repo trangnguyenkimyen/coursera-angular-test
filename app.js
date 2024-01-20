ac2b8d0327f536703cdd1de962455a7e2de053a0
Kim Yen <Trangnguyenkimyen@gmail.com>
2024-01-20 19:12:12 +0700

    Controller As Syntax

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 (function () {
     'use-strict';
 
-    const shoppingList = [
-        "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
-    ];
+    function ParentController1($scope) {
+        $scope.parentValue = 1;
+        $scope.pc = this;
+        $scope.pc.parentValue = 1;
+    }
 
-    const ShoppingListController = ($scope) => {
-        $scope.shoppingList = shoppingList;
+    function ChildController1($scope) {
+        console.log('$scope.parentValue: ', $scope.parentValue);
+        console.log('CHILD $scope: ', $scope);
+
+        $scope.parentValue = 5;
+        console.log('*** CHANGED: $scope.parentValue = 5 ***');
+        console.log('$scope.parentValue: ', $scope.parentValue);
+        console.log('CHILD $scope: ', $scope);
+
+        console.log('$scope.pc.parentValue: ', $scope.pc.parentValue);
+        $scope.pc.parentValue = 5;
+        console.log('*** CHANGED: $scope.pc.parentValue = 5 ***');
+        console.log('$scope.pc.parentValue: ', $scope.pc.parentValue);
+        console.log('CHILD $scope: ', $scope);
+
+        console.log('$scope.$parent.parentValue: ', $scope.$parent.parentValue);
     };
 
+    function ParentController2() {
+        const parent = this;
+        parent.value = 1;
+    };
 
-    angular.module('ShoppingListApp', [])
-        .controller('ShoppingListController', ShoppingListController);
+    function ChildController2($scope) {
+        const child = this;
+        child.value = 5;
+        console.log('ChildController2 $scope: ', $scope);
+    };
 
-    ShoppingListController.$inject = ['$scope'];
+    angular.module('ControllerAsApp', [])
+        .controller('ParentController1', ParentController1)
+        .controller('ChildController1', ChildController1)
+        .controller('ParentController2', ParentController2)
+        .controller('ChildController2', ChildController2);
+
+    ParentController1.$inject = ['$scope'];
+    ChildController1.$inject = ['$scope'];
+    ChildController2.$inject = ['$scope'];
 })();
