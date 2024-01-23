f0883232ded76fada8a2d8e4a8a7944e7bd0558d
Kim Yen <Trangnguyenkimyen@gmail.com>
2024-01-23 17:10:08 +0700

    first init

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 (function () {
     'use-strict';
 
-    angular.module('ControllerAsApp', [])
+    angular.module('ShoppingListPromiseApp', [])
         .controller('ShoppingListController', ShoppingListController)
-        .provider('ShoppingListService', ShoppingListServiceProvider)
-        .config(Config);
-
-    Config.$inject = ['ShoppingListServiceProvider'];
-    function Config(ShoppingListServiceProvider) {
-        ShoppingListServiceProvider.defaults.maxItems = 2;
-    };
+        .service('ShoppingListService', ShoppingListService)
+        .service('WeightLossFilterService', WeightLossFilterService);
 
     ShoppingListController.$inject = ['ShoppingListService'];
+
     function ShoppingListController(ShoppingListService) {
         const list = this;
-
 
         list.items = ShoppingListService.getItems();
 
         list.itemName = '';
-        list.quantity = '';
+        list.itemQuantity = '';
 
         list.addItem = function () {
             try {
-                ShoppingListService.addItem(list.itemName, list.quantity);
+                ShoppingListService.addItem(list.itemName, list.itemQuantity);
             } catch (error) {
                 list.errorMsg = error.message;
             }
@@ -34,22 +29,70 @@
         };
     };
 
-    function ShoppingListService(maxItems) {
+    ShoppingListService.$inject = ['$q', 'WeightLossFilterService'];
+
+    function ShoppingListService($q, WeightLossFilterService) {
         const service = this;
 
         const items = [];
 
-        service.addItem = function (itemName, quantity) {
-            if (!maxItems || (maxItems && items.length < maxItems)) {
-                const item = {
-                    name: itemName,
-                    quantity
-                };
+        // service.addItem = function (itemName, quantity) {
+        //     const promise = WeightLossFilterService.checkName(itemName);
 
-                items.push(item);
-            } else {
-                throw new Error("Max items (" + maxItems + ") reached.");
-            }
+        //     promise.then(function (response) {
+        //         const nextPromise = WeightLossFilterService.checkQuantity(quantity);
+
+        //         nextPromise.then(function (result) {
+        //             const item = {
+        //                 name: itemName,
+        //                 quantity: quantity
+        //             };
+
+        //             items.push(item);
+        //         }, function (error) {
+        //             console.log(error.message);
+        //         });
+        //     }, function (error) {
+        //         console.log(error.message);
+        //     });
+        // };
+
+        // service.addItem = function (name, quantity) {
+        //     const promise = WeightLossFilterService.checkName(name);
+
+        //     promise
+        //         .then(function (response) {
+        //             return WeightLossFilterService.checkQuantity(quantity);
+        //         })
+        //         .then(function (response) {
+        //             const item = {
+        //                 name: name,
+        //                 quantity: quantity
+        //             };
+
+        //             items.push(item);
+        //         })
+        //         .catch(function (error) {
+        //             console.log(error.message);
+        //         });
+        // };
+
+        service.addItem = function (name, quantity) {
+            const namePromise = WeightLossFilterService.checkName(name);
+            const quantityPromise = WeightLossFilterService.checkQuantity(quantity);
+
+            $q.all([namePromise, quantityPromise])
+                .then(function (response) {
+                    const item = {
+                        name: name,
+                        quantity: quantity
+                    };
+
+                    items.push(item);
+                })
+                .catch(function (error) {
+                    console.log(error.message);
+                });
         };
 
         service.removeItem = function (itemIndex) {
@@ -61,17 +104,49 @@
         };
     };
 
-    function ShoppingListServiceProvider() {
-        const provider = this;
+    WeightLossFilterService.$inject = ['$q', '$timeout'];
 
-        provider.defaults = {
-            maxItems: 10
+    function WeightLossFilterService($q, $timeout) {
+        const service = this;
+
+        service.checkName = function (name) {
+            const deferred = $q.defer();
+
+            let result = {
+                message: ''
+            };
+
+            $timeout(function () {
+                // Check for cookies
+                if (name.toLowerCase().indexOf('cookie') === -1) {
+                    deferred.resolve(result);
+                } else {
+                    result.message = 'Stay away from cookies, Yaakov!';
+                    deferred.reject(result);
+                }
+            }, 3000);
+
+            return deferred.promise;
         };
 
-        provider.$get = function () {
-            const shoppingList = new ShoppingListService(provider.defaults.maxItems);
+        service.checkQuantity = function (quantity) {
+            const deferred = $q.defer();
 
-            return shoppingList;
+            let result = {
+                message: ''
+            };
+
+            $timeout(function () {
+                // Check too many boxes
+                if (quantity < 6) {
+                    deferred.resolve(result);
+                } else {
+                    result.message = "That's too much, Yaakov!";
+                    deferred.reject(result);
+                }
+            }, 1000);
+
+            return deferred.promise;
         };
     };
 })();
